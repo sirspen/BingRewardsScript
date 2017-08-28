59a177fa932d1d32e1c3227fd7b40d0881c59df1
sirspen <sirspen@gmail.com>
2017-08-28 11:56:59 +0100

    Creating a main.js for my bing JavaScript project
    Initial commit with a shell for the JavaScript code. As of right now, all it does is check that the page is bing and create a random number

---
+++ main.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Bing Search
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Will attempt random bing searches
+// @author       Spencer Stantis
+// @match        https://www.bing.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    //creates a random bumber from 15 to 25 for the seconds timer
+    Math.floor(Math.random()*10+15);
+})();
